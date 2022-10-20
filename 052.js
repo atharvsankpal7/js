8ca14e75d1b78a9d58755c8a0d1c610e737dfac1
atharvsankpal7 <atharvsankpal69420@gmail.com>
2022-10-20 16:38:59 +0530

    hello

--- 052.js
+++ 052.js
@@ -1,7 +1,39 @@
-const prompt = require("prompt-sync")();
-let a = prompt("Enter your age");
-let b = prompt("Enter your gender ");
-let c = prompt("Enter your age ");
-a =Number.parseInt(a)// 
-c =Number.parseInt(c)
-console.log(a, b, c);
+// This is asynchronous program
+
+// const prompt = require("prompt-sync")();
+// let a = prompt("Enter your age");
+// let b = prompt("Enter your gender ");
+// let c = prompt("Enter your age ");
+// a =Number.parseInt(a)//
+// c =Number.parseInt(c)
+// console.log(a, b, c);
+
+//This is synchronous program
+
+// console.log("start");
+// setTimeout(function () {
+//     console.log("The function has ran");
+// }, 1000);
+// console.log("end");
+
+function loadscript(source, callback_function) {
+    let script = document.createElement("script");
+    script.src = source;
+    
+    document.body.appendChild(script);
+    callback_function();
+}
+loadscript(
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js",
+    hello
+);
+loadscript(
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js",
+    bye
+);
+function hello() {
+    console.log("hello");
+}
+function bye() {
+    console.log("bye");
+}
---
+++ demo.js
@@ -0,0 +1,608 @@
+function generate() {
+    var one = Math.floor(Math.random() * Math.floor(35));
+    var two = Math.floor(Math.random() * Math.floor(35));
+    var three = Math.floor(Math.random() * Math.floor(35));
+    var four = Math.floor(Math.random() * Math.floor(35));
+    var five = Math.floor(Math.random() * Math.floor(35));
+    var six = Math.floor(Math.random() * Math.floor(35));
+    var seven = Math.floor(Math.random() * Math.floor(35));
+    var eight = Math.floor(Math.random() * Math.floor(35));
+    if (one == 0) {
+        one = 0;
+    } else if (one == 1) {
+        one = 1;
+    } else if (one == 2) {
+        one = 2;
+    } else if (one == 3) {
+        one = 3;
+    } else if (one == 4) {
+        one = 4;
+    } else if (one == 5) {
+        one = 5;
+    } else if (one == 6) {
+        one = 6;
+    } else if (one == 7) {
+        one = 7;
+    } else if (one == 8) {
+        one = 8;
+    } else if (one == 9) {
+        one = 9;
+    } else if (one == 10) {
+        one = "q";
+    } else if (one == 11) {
+        one = "w";
+    } else if (one == 12) {
+        one = "e";
+    } else if (one == 13) {
+        one = "r";
+    } else if (one == 14) {
+        one = "t";
+    } else if (one == 15) {
+        one = "z";
+    } else if (one == 16) {
+        one = "u";
+    } else if (one == 17) {
+        one = "i";
+    } else if (one == 18) {
+        one = "o";
+    } else if (one == 19) {
+        one = "p";
+    } else if (one == 20) {
+        one = "a";
+    } else if (one == 21) {
+        one = "s";
+    } else if (one == 22) {
+        one = "d";
+    } else if (one == 23) {
+        one = "f";
+    } else if (one == 24) {
+        one = "g";
+    } else if (one == 25) {
+        one = "h";
+    } else if (one == 26) {
+        one = "j";
+    } else if (one == 27) {
+        one = "k";
+    } else if (one == 28) {
+        one = "l";
+    } else if (one == 29) {
+        one = "y";
+    } else if (one == 30) {
+        one = "x";
+    } else if (one == 31) {
+        one = "c";
+    } else if (one == 32) {
+        one = "v";
+    } else if (one == 33) {
+        one = "b";
+    } else if (one == 34) {
+        one = "n";
+    } else if (one == 35) {
+        one = "m";
+    }
+    if (two == 0) {
+        two = 0;
+    } else if (two == 1) {
+        two = 1;
+    } else if (two == 2) {
+        two = 2;
+    } else if (two == 3) {
+        two = 3;
+    } else if (two == 4) {
+        two = 4;
+    } else if (two == 5) {
+        two = 5;
+    } else if (two == 6) {
+        two = 6;
+    } else if (two == 7) {
+        two = 7;
+    } else if (two == 8) {
+        two = 8;
+    } else if (two == 9) {
+        two = 9;
+    } else if (two == 10) {
+        two = "q";
+    } else if (two == 11) {
+        two = "w";
+    } else if (two == 12) {
+        two = "e";
+    } else if (two == 13) {
+        two = "r";
+    } else if (two == 14) {
+        two = "t";
+    } else if (two == 15) {
+        two = "z";
+    } else if (two == 16) {
+        two = "u";
+    } else if (two == 17) {
+        two = "i";
+    } else if (two == 18) {
+        two = "o";
+    } else if (two == 19) {
+        two = "p";
+    } else if (two == 20) {
+        two = "a";
+    } else if (two == 21) {
+        two = "s";
+    } else if (two == 22) {
+        two = "d";
+    } else if (two == 23) {
+        two = "f";
+    } else if (two == 24) {
+        two = "g";
+    } else if (two == 25) {
+        two = "h";
+    } else if (two == 26) {
+        two = "j";
+    } else if (two == 27) {
+        two = "k";
+    } else if (two == 28) {
+        two = "l";
+    } else if (two == 29) {
+        two = "y";
+    } else if (two == 30) {
+        two = "x";
+    } else if (two == 31) {
+        two = "c";
+    } else if (two == 32) {
+        two = "v";
+    } else if (two == 33) {
+        two = "b";
+    } else if (two == 34) {
+        two = "n";
+    } else if (two == 35) {
+        two = "m";
+    }
+    if (three == 0) {
+        three = 0;
+    } else if (three == 1) {
+        three = 1;
+    } else if (three == 2) {
+        three = 2;
+    } else if (three == 3) {
+        three = 3;
+    } else if (three == 4) {
+        three = 4;
+    } else if (three == 5) {
+        three = 5;
+    } else if (three == 6) {
+        three = 6;
+    } else if (three == 7) {
+        three = 7;
+    } else if (three == 8) {
+        three = 8;
+    } else if (three == 9) {
+        three = 9;
+    } else if (three == 10) {
+        three = "q";
+    } else if (three == 11) {
+        three = "w";
+    } else if (three == 12) {
+        three = "e";
+    } else if (three == 13) {
+        three = "r";
+    } else if (three == 14) {
+        three = "t";
+    } else if (three == 15) {
+        three = "z";
+    } else if (three == 16) {
+        three = "u";
+    } else if (three == 17) {
+        three = "i";
+    } else if (three == 18) {
+        three = "o";
+    } else if (three == 19) {
+        three = "p";
+    } else if (three == 20) {
+        three = "a";
+    } else if (three == 21) {
+        three = "s";
+    } else if (three == 22) {
+        three = "d";
+    } else if (three == 23) {
+        three = "f";
+    } else if (three == 24) {
+        three = "g";
+    } else if (three == 25) {
+        three = "h";
+    } else if (three == 26) {
+        three = "j";
+    } else if (three == 27) {
+        three = "k";
+    } else if (three == 28) {
+        three = "l";
+    } else if (three == 29) {
+        three = "y";
+    } else if (three == 30) {
+        three = "x";
+    } else if (three == 31) {
+        three = "c";
+    } else if (three == 32) {
+        three = "v";
+    } else if (three == 33) {
+        three = "b";
+    } else if (three == 34) {
+        three = "n";
+    } else if (three == 35) {
+        three = "m";
+    }
+    if (four == 0) {
+        four = 0;
+    } else if (four == 1) {
+        four = 1;
+    } else if (four == 2) {
+        four = 2;
+    } else if (four == 3) {
+        four = 3;
+    } else if (four == 4) {
+        four = 4;
+    } else if (four == 5) {
+        four = 5;
+    } else if (four == 6) {
+        four = 6;
+    } else if (four == 7) {
+        four = 7;
+    } else if (four == 8) {
+        four = 8;
+    } else if (four == 9) {
+        four = 9;
+    } else if (four == 10) {
+        four = "q";
+    } else if (four == 11) {
+        four = "w";
+    } else if (four == 12) {
+        four = "e";
+    } else if (four == 13) {
+        four = "r";
+    } else if (four == 14) {
+        four = "t";
+    } else if (four == 15) {
+        four = "z";
+    } else if (four == 16) {
+        four = "u";
+    } else if (four == 17) {
+        four = "i";
+    } else if (four == 18) {
+        four = "o";
+    } else if (four == 19) {
+        four = "p";
+    } else if (four == 20) {
+        four = "a";
+    } else if (four == 21) {
+        four = "s";
+    } else if (four == 22) {
+        four = "d";
+    } else if (four == 23) {
+        four = "f";
+    } else if (four == 24) {
+        four = "g";
+    } else if (four == 25) {
+        four = "h";
+    } else if (four == 26) {
+        four = "j";
+    } else if (four == 27) {
+        four = "k";
+    } else if (four == 28) {
+        four = "l";
+    } else if (four == 29) {
+        four = "y";
+    } else if (four == 30) {
+        four = "x";
+    } else if (four == 31) {
+        four = "c";
+    } else if (four == 32) {
+        four = "v";
+    } else if (four == 33) {
+        four = "b";
+    } else if (four == 34) {
+        four = "n";
+    } else if (four == 35) {
+        four = "m";
+    }
+    if (five == 0) {
+        five = 0;
+    } else if (five == 1) {
+        five = 1;
+    } else if (five == 2) {
+        five = 2;
+    } else if (five == 3) {
+        five = 3;
+    } else if (five == 4) {
+        five = 4;
+    } else if (five == 5) {
+        five = 5;
+    } else if (five == 6) {
+        five = 6;
+    } else if (five == 7) {
+        five = 7;
+    } else if (five == 8) {
+        five = 8;
+    } else if (five == 9) {
+        five = 9;
+    } else if (five == 10) {
+        five = "q";
+    } else if (five == 11) {
+        five = "w";
+    } else if (five == 12) {
+        five = "e";
+    } else if (five == 13) {
+        five = "r";
+    } else if (five == 14) {
+        five = "t";
+    } else if (five == 15) {
+        five = "z";
+    } else if (five == 16) {
+        five = "u";
+    } else if (five == 17) {
+        five = "i";
+    } else if (five == 18) {
+        five = "o";
+    } else if (five == 19) {
+        five = "p";
+    } else if (five == 20) {
+        five = "a";
+    } else if (five == 21) {
+        five = "s";
+    } else if (five == 22) {
+        five = "d";
+    } else if (five == 23) {
+        five = "f";
+    } else if (five == 24) {
+        five = "g";
+    } else if (five == 25) {
+        five = "h";
+    } else if (five == 26) {
+        five = "j";
+    } else if (five == 27) {
+        five = "k";
+    } else if (five == 28) {
+        five = "l";
+    } else if (five == 29) {
+        five = "y";
+    } else if (five == 30) {
+        five = "x";
+    } else if (five == 31) {
+        five = "c";
+    } else if (five == 32) {
+        five = "v";
+    } else if (five == 33) {
+        five = "b";
+    } else if (five == 34) {
+        five = "n";
+    } else if (five == 35) {
+        five = "m";
+    }
+    if (six == 0) {
+        six = 0;
+    } else if (six == 1) {
+        six = 1;
+    } else if (six == 2) {
+        six = 2;
+    } else if (six == 3) {
+        six = 3;
+    } else if (six == 4) {
+        six = 4;
+    } else if (six == 5) {
+        six = 5;
+    } else if (six == 6) {
+        six = 6;
+    } else if (six == 7) {
+        six = 7;
+    } else if (six == 8) {
+        six = 8;
+    } else if (six == 9) {
+        six = 9;
+    } else if (six == 10) {
+        six = "q";
+    } else if (six == 11) {
+        six = "w";
+    } else if (six == 12) {
+        six = "e";
+    } else if (six == 13) {
+        six = "r";
+    } else if (six == 14) {
+        six = "t";
+    } else if (six == 15) {
+        six = "z";
+    } else if (six == 16) {
+        six = "u";
+    } else if (six == 17) {
+        six = "i";
+    } else if (six == 18) {
+        six = "o";
+    } else if (six == 19) {
+        six = "p";
+    } else if (six == 20) {
+        six = "a";
+    } else if (six == 21) {
+        six = "s";
+    } else if (six == 22) {
+        six = "d";
+    } else if (six == 23) {
+        six = "f";
+    } else if (six == 24) {
+        six = "g";
+    } else if (six == 25) {
+        six = "h";
+    } else if (six == 26) {
+        six = "j";
+    } else if (six == 27) {
+        six = "k";
+    } else if (six == 28) {
+        six = "l";
+    } else if (six == 29) {
+        six = "y";
+    } else if (six == 30) {
+        six = "x";
+    } else if (six == 31) {
+        six = "c";
+    } else if (six == 32) {
+        six = "v";
+    } else if (six == 33) {
+        six = "b";
+    } else if (six == 34) {
+        six = "n";
+    } else if (six == 35) {
+        six = "m";
+    }
+    if (seven == 0) {
+        seven = 0;
+    } else if (seven == 1) {
+        seven = 1;
+    } else if (seven == 2) {
+        seven = 2;
+    } else if (seven == 3) {
+        seven = 3;
+    } else if (seven == 4) {
+        seven = 4;
+    } else if (seven == 5) {
+        seven = 5;
+    } else if (seven == 6) {
+        seven = 6;
+    } else if (seven == 7) {
+        seven = 7;
+    } else if (seven == 8) {
+        seven = 8;
+    } else if (seven == 9) {
+        seven = 9;
+    } else if (seven == 10) {
+        seven = "q";
+    } else if (seven == 11) {
+        seven = "w";
+    } else if (seven == 12) {
+        seven = "e";
+    } else if (seven == 13) {
+        seven = "r";
+    } else if (seven == 14) {
+        seven = "t";
+    } else if (seven == 15) {
+        seven = "z";
+    } else if (seven == 16) {
+        seven = "u";
+    } else if (seven == 17) {
+        seven = "i";
+    } else if (seven == 18) {
+        seven = "o";
+    } else if (seven == 19) {
+        seven = "p";
+    } else if (seven == 20) {
+        seven = "a";
+    } else if (seven == 21) {
+        seven = "s";
+    } else if (seven == 22) {
+        seven = "d";
+    } else if (seven == 23) {
+        seven = "f";
+    } else if (seven == 24) {
+        seven = "g";
+    } else if (seven == 25) {
+        seven = "h";
+    } else if (seven == 26) {
+        seven = "j";
+    } else if (seven == 27) {
+        seven = "k";
+    } else if (seven == 28) {
+        seven = "l";
+    } else if (seven == 29) {
+        seven = "y";
+    } else if (seven == 30) {
+        seven = "x";
+    } else if (seven == 31) {
+        seven = "c";
+    } else if (seven == 32) {
+        seven = "v";
+    } else if (seven == 33) {
+        seven = "b";
+    } else if (seven == 34) {
+        seven = "n";
+    } else if (seven == 35) {
+        seven = "m";
+    }
+    if (eight == 0) {
+        eight = 0;
+    } else if (eight == 1) {
+        eight = 1;
+    } else if (eight == 2) {
+        eight = 2;
+    } else if (eight == 3) {
+        eight = 3;
+    } else if (eight == 4) {
+        eight = 4;
+    } else if (eight == 5) {
+        eight = 5;
+    } else if (eight == 6) {
+        eight = 6;
+    } else if (eight == 7) {
+        eight = 7;
+    } else if (eight == 8) {
+        eight = 8;
+    } else if (eight == 9) {
+        eight = 9;
+    } else if (eight == 10) {
+        eight = "q";
+    } else if (eight == 11) {
+        eight = "w";
+    } else if (eight == 12) {
+        eight = "e";
+    } else if (eight == 13) {
+        eight = "r";
+    } else if (eight == 14) {
+        eight = "t";
+    } else if (eight == 15) {
+        eight = "z";
+    } else if (eight == 16) {
+        eight = "u";
+    } else if (eight == 17) {
+        eight = "i";
+    } else if (eight == 18) {
+        eight = "o";
+    } else if (eight == 19) {
+        eight = "p";
+    } else if (eight == 20) {
+        eight = "a";
+    } else if (eight == 21) {
+        eight = "s";
+    } else if (eight == 22) {
+        eight = "d";
+    } else if (eight == 23) {
+        eight = "f";
+    } else if (eight == 24) {
+        eight = "g";
+    } else if (eight == 25) {
+        eight = "h";
+    } else if (eight == 26) {
+        eight = "j";
+    } else if (eight == 27) {
+        eight = "k";
+    } else if (eight == 28) {
+        eight = "l";
+    } else if (eight == 29) {
+        eight = "y";
+    } else if (eight == 30) {
+        eight = "x";
+    } else if (eight == 31) {
+        eight = "c";
+    } else if (eight == 32) {
+        eight = "v";
+    } else if (eight == 33) {
+        eight = "b";
+    } else if (eight == 34) {
+        eight = "n";
+    } else if (eight == 35) {
+        eight = "m";
+    }
+    var code = `${one}${two}${three}${four}${five}${six}${seven}${eight}`;
+    var times = Math.floor(Math.random() * Math.floor(8));
+    var which = Math.floor(Math.random() * Math.floor(8));
+    var i = 0;
+    while (i < times) {
+        code = code.replace(code[which], code[which].toUpperCase());
+        which = Math.floor(Math.random() * Math.floor(8));
+        i++;
+    }
+    return code;
+}
+
+document.getElementById("generate").onclick = function () {
+    document.getElementById("generated").value = generate();
+};
