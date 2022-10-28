766abfcf3b9121dc8ccae436163ebf91df05b2ac
atharvsankpal7 <atharvsankpal69420@gmail.com>
2022-10-29 00:11:02 +0530

    hello

--- 067.js
+++ 067.js
@@ -1,24 +1,36 @@
-const creattodo = async (todo) => {
-    let options = {
-        method: "post",
-        headers: {
-            "Content-type": "application/json",
-        },
-        body: JSON.stringify(todo),
+mainFunction = async () => {
+    const creattodo = async (todo) => {
+        let options = {
+            method: "post",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(todo),
+        };
+        let p = await fetch(
+            "https://jsonplaceholder.typicode.com/posts  ",
+            options
+        );
+        return p.json();
     };
-    let p = await fetch(
-        "https://jsonplaceholder.typicode.com/posts/1  ",
-        options
-    );
-    return await p.json();
-};
-const mainfunc = async () => {
-    let todo = {
-        title: "Harry2",
-        body: "bhai2",
-        userId: 1100,
+    const gettodo = async (id) => {
+        let response = await fetch(
+            "https://jsonplaceholder.typicode.com/posts/" + id
+        );
+        let r = await response.json();
+        return r;
     };
-    let todor = await creattodo(todo);
-    console.log(todor);
+    const extrafunction = async () => {
+        let todo = {
+            title: "Harry2",
+            body: "bhai2",
+            userId: 1100,
+        };
+        let todor = await creattodo(todo);
+        console.log(todor);
+        console.log(await gettodo(5));
+    };
+    extrafunction();
+    console.log(await gettodo(6));
 };
-mainfunc();
+mainFunction();
---
+++ 068.js
@@ -0,0 +1,7 @@
+console.log(document.cookie);
+document.cookie = "name=Atharv";
+console.log(document.cookie);
+document.cookie = "ID=1002";
+console.log(document.cookie);
+document.cookie = "name=NewName";
+console.log(document.cookie);
